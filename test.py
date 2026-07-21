9d2fa146ca7e9b4a7d804b451f6ca72648b447c4
amank158 <155568064+amank158@users.noreply.github.com>
2026-07-21 15:32:14 +0530

    modified test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(6)
+a=7,'p',9
+print(type(a))
+
+# try to find out vowel from sentence 
+sentence="Hello,how are you?"
+vowels="aeiouAEIOU"
+a=[i for i in sentence if i in vowels]
+print(a)
+
+
